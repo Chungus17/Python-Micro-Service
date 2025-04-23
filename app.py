2bc0d6b9f7abfb940e89c01b7c7b5d6159232fa9
Chungus17 <haroon.n.shah1999@gmail.com>
2025-04-23 15:24:03 +0300

    CORS added

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # 👈 This enables CORS for all domains (or specify origins)
 
 @app.route('/')
 def home():
