54348949c43b1130a5eae7d0f4136cf8b1b991f7
Chungus17 <haroon.n.shah1999@gmail.com>
2025-05-15 16:47:24 +0300

    Latest commit

--- app.py
+++ app.py
@@ -94,14 +94,9 @@ def send_email(excel_file, subject, clientName):
 @app.route('/data_analysis', methods=["POST"])
 def data_analysis():
 
-    forwarded_for = request.headers.get("X-Forwarded-For", request.remote_addr)
-    client_ip = forwarded_for.split(",")[0].strip()
-
-    print(client_ip)
-
-    # Check if the request is coming from the allowed IP address
-    if client_ip != ALLOWED_IP:
-        abort(403, description="Access forbidden: IP not allowed")
+    referer = request.headers.get("Referer", "")
+    if not referer.startswith(ALLOWED_IP):
+        abort(403, description="Access forbidden: Referer not allowed")
 
     params_data = request.get_json()
     start_date = params_data.get('start_date')
